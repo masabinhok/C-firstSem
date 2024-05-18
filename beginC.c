621336e847c3a495117d11b1127fcf54159cbb3f
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-05-18 18:47:53 +0545

    Q10 accomplished, modelQuestion2080 solved

--- beginC.c
+++ beginC.c
@@ -2,5 +2,6 @@
 
 int main () {
   printf("The red lines under my code isn't beautful!");
+  printf("Lets start C ing with fingers!");
   return 0;
 }
---
+++ modelQuestion2080/10.c
@@ -0,0 +1,14 @@
+//Difference between Proceduaral Oriented Programming and Object Oriented Programming
+
+/*Procedural oriented programming and object oriented programming are two different approaches to programming, each with its own characterstics and principles. Here's a brief differentiation betweeen the two:
+
+1. POP emphasizes procedures or functions as the primary building blocks of programs, wheras OOP emphasizes objects as the primary building blocks of programs.
+
+2. In POP, data and functions are treated as seperate entities, while in OOP, data and functions are bundled together within objects.
+
+3. POP typically follows a top-down approach, breaking down problems into smaller tasks, wheras OOP follows a bottom-up appoach, defining and modelling objects and their interactions.
+
+4. Global data is heavily relied upon and can be accessed and modified by any function in POP, wheras encapsulation is promoted in OOP, hiding internal details and providing abstraction.
+
+5. Finally, OOP supports inheritance, allowing classes to inherit properties and behaviour from other classes, promoting code reuse and modularity, which is not a characterstics of POP.
+*/
